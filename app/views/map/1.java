7056a6978edc6db1334bdc13427a13ebc6c6d990
EC2 Default User <ec2-user@ip-172-31-44-111.ap-northeast-1.compute.internal>
2023-04-08 13:43:39 +0000

    java4

--- app/views/map/1.java
+++ app/views/map/1.java
@@ -9,3 +9,26 @@ public class Main {
 	    }
 	}
 }
+
+public class Main {
+	public static void main(String[] args) { 
+	    boolean tenki = true;
+	    if (tenki == true) {
+	        System.out.println ("洗濯物をする");
+	        System.out.println ("散歩をする");
+	    } else
+	        System.out.println ("DVDを見ます");
+	}
+}
+
+
+public class Main {
+	public static void main(String[] args) { 
+	    boolean boorClose = true;
+	    while (doorClose == true){
+	        System.out.println("ノックする");
+	        System.out.println("1分待つ");
+	    }
+	    
+	}
+}

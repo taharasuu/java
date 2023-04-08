2ab2b45b4df12069c31aaef057265225475b5124
EC2 Default User <ec2-user@ip-172-31-44-111.ap-northeast-1.compute.internal>
2023-04-08 14:42:55 +0000

    java6

--- app/views/map/1.java
+++ app/views/map/1.java
@@ -67,3 +67,12 @@ public class Main {
     }
   }
 }
+
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 3; i++) {
+      System.out.print("現在" + (i + 1) + "周目→");
+    }
+  }
+}
+

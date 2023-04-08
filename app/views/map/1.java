7bdac622567d7a6934f53d7d99de2d4829d63183
EC2 Default User <ec2-user@ip-172-31-44-111.ap-northeast-1.compute.internal>
2023-04-08 12:13:22 +0000

    java2

---
+++ app/views/map/1.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) { 
+	    boolean tenki = false;
+	    if (tenki == true) {
+	        System.out.println("洗濯をします");
+	        System.out.println("散歩に行きます");
+	    } else {
+	        System.out.println("DVDを見ます");
+	    }
+	}
+}

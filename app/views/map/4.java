7df42745164f4541ce793576f8af9a8899d855ca
taharasuu <110362096+taharasuu@users.noreply.github.com>
2023-04-15 09:34:29 +0000

    java5

--- app/views/map/4.java
+++ app/views/map/4.java
@@ -3,12 +3,12 @@
 public class Main {
   public static void main(String[] args) {
     int [] seq = new int[10];
-    
+
     //塩基をランダムに生成
     for (int i = 0; i < seq.length; i++) {
       seq[i] = new java.util.Random().nextInt(4);
     }
-    
+
     //生成した塩基配列の希望を表示
     for (int i = 0; i < seq.length; i++) {
       switch (seq[i]) {
@@ -33,15 +33,72 @@ public class Main {
 public class Main {
   public static void main(String[] args) {
     int [] seq = new int[10];
-    
+
     //塩基をランダムに生成
     for (int i = 0; i < seq.length; i++) {
       seq[i] = new java.util.Random().nextInt(4);
     }
-    
+
     //生成した塩基配列の希望を表示
     for (int i = 0; i < seq.length; i++) {
     char [] base = {"A", "T", "C", "G"};
     System.out.print(base[seq[i]] + "");
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    int[] scores = {20, 30, 40, 50, 80};
+    for (int value : scores) {
+      System.out.println(value);
+    }
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    int[] arrayA = {1, 2, 3};
+    int[] arrayB;
+    arrayB = arrayA;
+    arrayB[0] = 100;
+    System.out.println(arrayA[0]);
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    boolean judge = true;
+    if (judge == ture) {
+      int[] array = {1, 2, 3};
+    }
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    int[] array = {1, 2, 3};
+    array = null;
+    array[0] = 10;
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    int[] [] scores = new int[2] [3];
+    scores[0] [0] = 40;
+    scores[0] [1] = 50;
+    scores[0] [2] = 60;
+    scores[1] [0] = 80;
+    scores[1] [1] = 60;
+    scores[1] [2] = 70;
+    System.out.println(scores[1] [1]);
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    int[] [] scores = { {40, 50, 60}, {80, 60, 70}};
+    System.out.println(scores.length);
+    System.out.println(scores[0].length);
   }
 }
---
+++ app/views/map/5.java
@@ -0,0 +1,117 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("メソッドを呼び出しています");
+    hello("湊");
+    hello("朝香");
+    hello("菅原");
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void main(String name) {
+    System.out.println(name + "さん、こんにちは")
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    add(100, 20);
+    add(200, 50);
+  }
+  public static void add(int x, int y) {
+    int ans = x + y;
+    System.out.println(x + "+" + y + "=" + ans);
+  }
+}
+
+
+public class Main {
+  public static void add(int x, int y) {
+    int ans = x + y;
+    return ans;
+  }
+  public static void main(String[] args) {
+    int ans = add(100, 10);
+    System.out.println("100 + 10 =" + ans);
+  }
+}
+
+public class Main {
+  public static int add(int x, int y) {
+    int ans = x + y;
+    return ans;
+  }
+  public static void main(String[] args) {
+    System.out.println(add(add(10, 20), add(30, 40)));
+  }
+}
+
+public class Main {
+  public static int add(int x, int y) {
+    return x + y;
+  }
+  public static double add(double x, double y) {
+    return x + y;
+  }
+  public static String add(String x, String y) {
+    return x + y;
+  }
+  public static void main(String[] args) {
+    System.out.println(add(10, 20));
+    System.out.println(add(3.5, 2.7));
+    System.out.println(add("Hello", "World"));
+  }
+}
+
+public class Main {
+  public static int add(int x, int y) {
+    return x + y;
+  }
+  public static int add(int x, int y, int z) {
+    return x + y;
+  }
+  public static void main(String[] args) {
+    System.out.println("10+20=" + add(10, 20));
+    System.out.println("10+20+30=" + add(10, 20, 30));
+  }
+}
+
+public class Main {
+  public static void printArray(int[] array) {
+    for (int element : array){
+      System.out.println(element);
+    }
+  }
+  public static void main(String[] args) {
+    int [] array = {1, 2, 3};
+    printArray(array);
+  }
+}
+
+public class Main {
+  public static void inctArray(int[] array) {
+    for (int i = 0; i < array.length; i++ ) {
+      array[i]++;
+    }
+  }
+  public static void main(String[] args) {
+    int [] array = {1, 2, 3};
+    for (int i : array) {
+      System.out.println(i);
+    }
+  }
+}
+
+public class Main {
+  public static int[] makeArray(int size) {
+    int [] newArray = new int[size];
+    for (int i = 0; i < newArray.length; i++) {
+        newArray[i] = i;
+    }
+    return newArray;
+  }
+  public static void main(String[] args) {
+    int [] array = makeArray(3);
+    for (int i : array) {
+      System.out.println(i);
+    }
+  }
+}

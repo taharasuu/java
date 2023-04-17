449ec2e2aa4859c32b3518bc2e0602e5be795f26
taharasuu <110362096+taharasuu@users.noreply.github.com>
2023-04-17 14:28:14 +0000

    java6-10

--- app/views/1~6shou/6.java
+++ app/views/1~6shou/6.java
@@ -33,3 +33,14 @@ public class CalcLogic {
     return (a - b);
   }
 }
+
+//6-10 API利用
+public class Main {
+  public static void main(String[] args) {
+    int[] heights = {172, 149, 152, 191, 155};
+    java.util.Arrays.sort(heights);
+    for (int h : heights) {
+      System.out.println(h);
+    }
+  }
+}

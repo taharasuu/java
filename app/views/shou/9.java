fc98cdd29a2d05e261094196add11de2865a6998
taharasuu <110362096+taharasuu@users.noreply.github.com>
2023-04-23 14:40:08 +0000

    9-4

--- app/views/shou/9.java
+++ app/views/shou/9.java
@@ -10,6 +10,43 @@ public class Main {
     h = new Hero();
     //フィールドへの値の代入（参照の解決・アドレス解決
     h.hp = 100;
+    Hero h2;
+    h2 = h1;
+    h2.hp = 200;
+    System.out.println(h1.hp);
   }
 }
 
+//9-3 sword型フィールドをもつHeroクラス
+//ますswordクラスを定義する
+//Sword.java
+public class Sword {
+  String name;
+  int damage;
+}
+
+//次にHeroクラスを定義
+public class Hero {
+  String name;
+  int hp;
+  Sword sword;
+  public void attack() {
+    System.out.println(this.name + "は攻撃した！");
+    System.out.println("敵に５ポイントのダメージを与えた！");
+  }
+ }
+ 
+ 
+ //9-4 剣を装備した勇者を生み出す
+ public class Main {
+  public static void main(String[] args) {
+    Sword s = new Sword();
+    s.name = "炎の剣";
+    s.damage = 10;
+    Hero h = new Hero();
+    h.name = "ミナト";
+    h.hp = 100;
+    h.sword = s;
+    System.out.println("現在の武器は" + h.sword.name);
+  }
+}

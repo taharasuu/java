4b34dce8a4d3a17783881ece1afb3ff2b88a47d6
taharasuu <110362096+taharasuu@users.noreply.github.com>
2023-05-02 13:50:00 +0000

    12-10

--- app/views/shou/12.java
+++ app/views/shou/12.java
@@ -34,3 +34,73 @@ public class Main {
     w.fireball(m);
   }
 }
+
+//12-6 パーティが夙夜に泊まってHPを回復する
+public class Main {
+  public static void main(String[] args) {
+    Hero h1 = new Hero();
+    Hero h2 = new Hero();
+    Thief t1 = new Thief();
+    Wizare w1 = new Wizard();
+    Wizare w2 = new Wizard();
+
+    h1.hp += 50;
+    h2.hp += 50;
+    t1.hp += 50;
+    w1.hp += 50;
+    w2.hp += 50;
+  }
+}
+
+//12-7多態性と配列を君あわせて宿屋に泊まる
+public class Main {
+  public static void main(String[] args) {
+    Character[] c = new Character[5];
+    c[0] = new Hero();
+    c[1] = new Hero();
+    c[2] = new Thief();
+    c[3] = new Wizard();
+    c[4] = new Wizard();
+    
+    for (Character ch : c) {
+      ch.hp += 50;
+    }
+  }
+}
+
+//12-8 攻撃する相手ごとにattack()を作成
+public class Hero extends Character { //お化けきのこの攻撃用
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃！");
+    System.out.println("的に10ポイントのダメージを与えた!");
+    m.hp -= 10;
+  }
+  public void attack(Goblin g) {  //ゴブリンの攻撃用
+    System.out.println(this.name + "の攻撃！");
+    System.out.println("的に10ポイントのダメージを与えた！");
+    g.hp -= 10;
+  }
+}
+
+//12-9 Monsterならなんでも攻撃できるattackメソッド
+public class Hero extends Character {
+  public void attack(Monster m) {  //モンスター攻撃用
+    System.out.println(this.name + "の攻撃！");
+    System.out.println("的に10ポイントのダメージを与えた!");
+    m.hp -= 10;
+  }
+}
+
+//12-10 同じ指示で異なる動作をさせることができる
+public class Main {
+  public static void main(String[] args) {
+    Monster[] monsters = new Monster[3];
+    monster[0] = new Slime();
+    monster[1] = new Goblin();
+    monster[2] = new DeathBat();
+    
+    for (Monster m : monsters) {
+      m.run();
+    }
+  }
+}

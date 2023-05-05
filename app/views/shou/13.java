63ddccc77ce805a265f34db9c17302321959c2ed
taharasuu <110362096+taharasuu@users.noreply.github.com>
2023-05-05 08:11:51 +0000

    java13-10

--- app/views/shou/13.java
+++ app/views/shou/13.java
@@ -18,17 +18,33 @@ public class Hero {
     System.out.println(this.name + "は死んでしまった！");
     System.out.println("GAME OVWEです")
   }
-  
   public void sleep() {
     this.hp = 100;
     System.out.println(this.name + "は、眠ってしまった！");
   }
-  
   public void attack(Matango m) {
     
   }
-  
 }
 
 
+//13-7.8.9.10 王様クラスで利用されるname
+//ヒーロクラスでprivate設定
 
+/*getterメソッド　フィールドちを取り出すだけのメソッド*/
+/*setterメソッド　フィールド値に値を代入するだけのメソッド*/
+public class Hero {
+  private String name;
+  
+  public String getName() { /*ゲッターメソッド*/
+    return this.name;
+  }
+  public void setName(String name) { /*セッターメソッド*/
+    this.name = name;
+  }
+}
+public class King {
+  void talk(Hero h) {
+    System.out.println("ようこそ若国へ、勇者" + h.getName + "よ。");
+  }
+}

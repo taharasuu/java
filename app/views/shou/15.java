78188687f9e510b0e82e3e00f5137f2f68da2e1c
taharasuu <110362096+taharasuu@users.noreply.github.com>
2023-05-11 15:21:38 +0000

    15.java

--- app/views/shou/15.java
+++ app/views/shou/15.java
@@ -33,3 +33,67 @@ public class Main {
     
   }
 }
+
+//15-3 文字列切り出しメソッドを利用した例
+public class Main {
+  public static void main(String[] args) {
+    String s1 = "Java programming";
+    System.out.println("文字列s1の4文字目以降は" + s1.substring(3));
+    System.out.println("文字列s1の4~8文字目は" + s1.substring(3, 8));
+  }
+}
+
+//15-4 stringbuiderを利用して文字列を１万回連結する
+public class Main {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10000; i++) {
+      sb.append("Java");
+    }
+    String s = sb.toString();
+  }
+}
+
+//15-5 正当なプレイヤー名であるかを判定するメソッド
+public boolean isValidPlayerName(String name) {
+  if (name.length() != 8) {
+    return fales;
+  }
+  char first = name.charAt(0);
+  if (!(first >= "A" && first <= "Z")) {
+    return false;
+  }
+  for (int i = 1; i < 8; i++) {
+    char c = name.charAt(i);
+    if (!((c >= "A" && c <= "Z") || (c >= "0" && c <= "9"))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+//15-6 文字列パターンを用意たプレイヤー名のチェック
+public boolean isValidPlayerName(String name) {
+  return name.matdches("[A-Z][A-ZO-9]{7}");
+}
+
+//15-7 spiltメソッドを使った文字列の分割
+public class Main {
+  public static void main(String[] args) {
+    String s = "abc,def:ghi";
+    String[] words = s.split("[,:]");
+    for (String w : words) {
+      System.out.println(w + "->");
+    }
+  }
+}
+
+//15-8 replaceAllメソッドを使った文字列の置換
+public class Main {
+  public static void main(String[] args) {
+    String s = "abc,def:ghi";
+    String w = s.replaceAll("[beh]","X");
+    System.out.pritnln(w);
+  }
+}

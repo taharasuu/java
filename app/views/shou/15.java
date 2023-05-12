502d3d108e81669686fa522537eaf5b4ec9bdbd5
taharasuu <110362096+taharasuu@users.noreply.github.com>
2023-05-12 10:46:19 +0000

    15-14

--- app/views/shou/15.java
+++ app/views/shou/15.java
@@ -126,3 +126,47 @@ public class Main {
     System.out.println(past);
   }
 }
+
+
+//15-12 6つのint値とDate型の相互変換
+import java.util.Calender;
+import java.until.Date;
+
+public class Main {
+  public static boid main(String[] args) {
+    Calendar c = Calender.getInstance();
+    c.set(2019,8,22,1,23,45);
+    c.set(Calendar.MONTH, 9); /*月は0=1月 10=11月 7=8月*/　
+    Date d = c.getTime();
+    System.out.println(d);
+    Date now = new Date();
+    c.setTime(now);
+    int y = c.get(Calendar.YEAR);
+    System.out.println("今年は" + y + "年です");
+  }
+}
+
+//15-13 String型とDate型の相互変換
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    SimleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    Date d = f.parse("2020/09/22 01:23:45");
+    System.out.println(d);
+    Date now = new Date();
+    String s = f.format(now);
+    System.out.println("現在は" + s + "です");
+  }
+}
+
+//15-14 InstantおよびZonedDateTimeの利用例
+import java.taim.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Instant i1 = Instant.now();
+    Instant i2 = Instant.ofEpochMilli(1600705425827L);
+  }
+}
